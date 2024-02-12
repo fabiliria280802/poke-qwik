5b39eb241c2e9eabc455eb58fe70ed10c9499a03
Fabiana Liria Soto <117101392+fabiliria280802@users.noreply.github.com>
2024-02-12 10:12:04 -0500

    Update currentPage value in PokemonProvider

--- src/context/pokemon/pokemon-provider.tsx
+++ src/context/pokemon/pokemon-provider.tsx
@@ -23,7 +23,7 @@ export const PokemonProvider = component$(() => {
   });
 
   const pokemonList = useStore<PokemonListState>({
-    currentPage: 1,
+    currentPage: 0,
     isLoading: false,
     pokemons: [],
   });
